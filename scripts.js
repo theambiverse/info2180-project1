07312656cf57ba2eb33001d9126649e44736b895
Nikita Clemetson <nikita.clemetson@mymona.uwi.edu>
2020-11-01 19:24:36 -0500

    Prelim version

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-/* Add your JavaScript to this file */
+"use strict";
+document.addEventListener("DOMContentLoaded", function(){
+
+    function submitform(){
+
+      let forms=  document.forms[0];
+
+      forms.addEventListener('submit', function(eve){
+        eve.preventDefault();
+        var val=document.getElementsByTagName('input')[0].value;
+        var exp=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (exp.test(val)){
+            var mes =document.querySelector(".message");
+            mes.innerHTML="Thank you! Your email address "+val+ " has been added to our mailing list!";
+            document.querySelector(".message").style.textAlign="center";
+            // forms.submit();
+
+        }
+         else{
+         document.getElementsByTagName('input')[0].focus();
+          var mes =document.querySelector(".message");
+            mes.innerHTML="Please enter a valid email address.";
+            document.querySelector(".message").style.textAlign="center";
+         }
+          
+      })
+
+    }
+    submitform();
+
+
+});
